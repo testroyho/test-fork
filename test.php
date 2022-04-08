0cd9eeca9b15edd271a5aa7dba636840a76155a1
roykho <roykho77@gmail.com>
2022-04-08 12:10:20 -0700

    Add version

--- test.php
+++ test.php
@@ -1,8 +1,10 @@
 <?php
 /**
  * Test main file..
- * Version: 1.0.0
+ * Version: 1.10.27
  *
  * @package Test
  * * Woo: 675790:79ca7aadafe706364e2d738b7c1090c4
  */
+
+define( 'WC_STORE_CATALOG_PDF_DOWNLOAD_VERSION', '1.10.27' ); // WRCS: DEFINED_VERSION.
